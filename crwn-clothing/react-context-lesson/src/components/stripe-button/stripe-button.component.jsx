3350d19f415f0a4c8cd81563e1f353e6c21d129e
Kalin <k.primov92@gmail.com>
2020-04-07 10:07:17 +0300

    dependency fixes and added context lesson

--- crwn-clothing/react-context-lesson/src/components/stripe-button/stripe-button.component.jsx
+++ crwn-clothing/react-context-lesson/src/components/stripe-button/stripe-button.component.jsx
@@ -1,25 +1,13 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
-import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
-  const publishableKey = 'pk_test_DpO9KGtGv1kSOpDn2HMFDDrC00f2H7pFdJ';
+  const publishableKey = 'pk_test_WBqax2FWVzS9QlpJScO07iuL';
 
   const onToken = token => {
-    axios({
-      url: 'payment',
-      method: 'post',
-      data: {
-        amount: priceForStripe,
-        token
-      }
-    }).then(response => {
-      alert('Payment successful')
-    }).catch(error => {
-      console.log('Payment error: ', JSON.parse(error));
-      alert('There was an issue with your payment. Please use the test credit card');
-    });
+    console.log(token);
+    alert('Payment Succesful!');
   };
 
   return (
