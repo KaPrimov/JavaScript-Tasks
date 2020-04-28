6436a216d01dbe4dac9053c62a25a11f47133eef
KaPrimov <k.primov92@gmail.com>
2020-04-28 18:07:18 +0300

    Added gatsby and webpack exercises

---
+++ gatsby-blog/src/pages/index.js
@@ -0,0 +1,56 @@
+import React from "react"
+import { graphql, Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import styled from "styled-components"
+
+const BlogLink = styled(Link)`
+  text-decoration: none;
+`
+const BlogTitle = styled.h3`
+  margin-bottom = 20;
+  color: blue;
+`;
+
+export default ({ data }) => (
+  <Layout>
+    <SEO title="Home" />
+    <div>
+      <h1>Blog Posts</h1>
+      <h4>{data.allMarkdownRemark.totalCount}</h4>
+      {data.allMarkdownRemark.edges.map(({ node }) => (
+        <div key={node.id}>
+          <BlogLink to={node.fields.slug}>
+            <BlogTitle>
+              {node.frontmatter.title} - {node.frontmatter.date}
+            </BlogTitle>
+          </BlogLink>
+          <p>{node.excerpt}</p>
+        </div>
+      ))}
+    </div>
+  </Layout>
+)
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      totalCount
+      edges {
+        node {
+          id
+          excerpt
+          frontmatter {
+            description
+            title
+            date
+          }
+          fields {
+            slug
+          }
+        }
+      }
+    }
+  }
+`
